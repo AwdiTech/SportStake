c6ff26b9d14d5fd50286c6b0a7ad5ea984c8ffe4
Abdulaziz Hassan <abdu2812.h@gmail.com>
2024-11-28 19:28:38 -0500

    see bets for a selected match

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,6 @@
 import "./App.css";
 import { useEffect, useState } from "react";
-import {
-    BrowserRouter as Router,
-    Routes,
-    Route,
-    Navigate,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import ProtectedRoute from "./ProtectedRoute";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home/Home";
@@ -21,126 +16,129 @@ import { get, ref } from "firebase/database";
 import { onAuthStateChanged } from "firebase/auth"; // Import the auth state change listener
 import HelpButton from "./components/HelpButton";
 import AdminConsole from "./pages/AdminConsole/AdminConsole.jsx";
+import Bets from "./pages/Bets/Bets.jsx";
 
 function App() {
-    const [isLoggedIn, setIsLoggedIn] = useState(null); // Store auth state
-    const [isAdmin, setIsAdmin] = useState(false); // Admin state
+  const [isLoggedIn, setIsLoggedIn] = useState(null); // Store auth state
+  const [isAdmin, setIsAdmin] = useState(false); // Admin state
 
-    useEffect(() => {
-        // Listen for authentication state changes
-        const unsubscribe = onAuthStateChanged(auth, async (user) => {
-            if (user) {
-                setIsLoggedIn(true); // User is logged in
-                // Fetch user details from Firebase
-                const dbRef = ref(db, `users/${user.uid}`);
-                const snapshot = await get(dbRef);
-                if (snapshot.exists()) {
-                    const userData = snapshot.val();
-                    setIsAdmin(userData.isAdmin || false); // Determine admin status
+  useEffect(() => {
+    // Listen for authentication state changes
+    const unsubscribe = onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        setIsLoggedIn(true); // User is logged in
+        // Fetch user details from Firebase
+        const dbRef = ref(db, `users/${user.uid}`);
+        const snapshot = await get(dbRef);
+        if (snapshot.exists()) {
+          const userData = snapshot.val();
+          setIsAdmin(userData.isAdmin || false); // Determine admin status
+        }
+      } else {
+        setIsLoggedIn(false); // User is logged out
+        setIsAdmin(false);
+      }
+    });
+
+    // Cleanup the listener on unmount
+    return () => unsubscribe();
+  }, []);
+
+  // Show a loading state while the auth status is being determined
+  if (isLoggedIn === null) {
+    return <p>Loading...</p>;
+  }
+  // App routing
+  // The `Router` component is the parent component that wraps the `Routes` and `Route` components.
+  // The `Routes` component is a container for multiple `Route` components.
+  // The `Route` component is used to define a route and its corresponding component.
+  // Add more `Route` components to define more pages in the Single-Page-Application.
+  return (
+    <div className="App">
+      <Router>
+        {isLoggedIn && <Navbar isAdmin={isAdmin} />}
+        {!isAdmin && isLoggedIn && <HelpButton />}
+        <Routes>
+          {/* Admin routes */}
+          {isAdmin && (
+            <>
+              <Route
+                path="/adminMessage"
+                element={
+                  <ProtectedRoute>
+                    <AdminMessage />
+                  </ProtectedRoute>
                 }
-            } else {
-                setIsLoggedIn(false); // User is logged out
-                setIsAdmin(false);
-            }
-        });
+              />
+              <Route
+                path="/stats"
+                element={
+                  <ProtectedRoute>
+                    <Stats />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/AdminConsole"
+                element={
+                  <ProtectedRoute>
+                    <AdminConsole />
+                  </ProtectedRoute>
+                }
+              />
+              {/* Redirect non-admin routes to adminMessage for admins */}
+              <Route path="*" element={<Navigate to="/adminMessage" />} />
+            </>
+          )}
 
-        // Cleanup the listener on unmount
-        return () => unsubscribe();
-    }, []);
+          {/* Non-admin routes */}
+          {!isAdmin && (
+            <>
+              <Route path="/" element={isLoggedIn ? <Home /> : <Login />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Registration />} />
+              <Route
+                path="/home"
+                element={
+                  <ProtectedRoute>
+                    <Home />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/matchList"
+                element={
+                  <ProtectedRoute>
+                    <MatchList />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/matchDetails/:eventId"
+                element={
+                  <ProtectedRoute>
+                    <Bets />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/profile/:id"
+                element={
+                  <ProtectedRoute>
+                    <Profile />
+                  </ProtectedRoute>
+                }
+              />
+              {/* Redirect admin-only routes to Home for non-admins */}
+              <Route path="*" element={<Navigate to="/home" />} />
+            </>
+          )}
 
-    // Show a loading state while the auth status is being determined
-    if (isLoggedIn === null) {
-        return <p>Loading...</p>;
-    }
-    // App routing
-    // The `Router` component is the parent component that wraps the `Routes` and `Route` components.
-    // The `Routes` component is a container for multiple `Route` components.
-    // The `Route` component is used to define a route and its corresponding component.
-    // Add more `Route` components to define more pages in the Single-Page-Application.
-    return (
-        <div className="App">
-            <Router>
-                {isLoggedIn && <Navbar isAdmin={isAdmin} />}
-                {!isAdmin && isLoggedIn && <HelpButton />}
-                <Routes>
-                    {/* Admin routes */}
-                    {isAdmin && (
-                        <>
-                            <Route
-                                path="/adminMessage"
-                                element={
-                                    <ProtectedRoute>
-                                        <AdminMessage />
-                                    </ProtectedRoute>
-                                }
-                            />
-                            <Route
-                                path="/stats"
-                                element={
-                                    <ProtectedRoute>
-                                        <Stats />
-                                    </ProtectedRoute>
-                                }
-                            />
-                            <Route
-                                path="/AdminConsole"
-                                element={
-                                    <ProtectedRoute>
-                                        <AdminConsole />
-                                    </ProtectedRoute>
-                                }
-                            />
-                            {/* Redirect non-admin routes to adminMessage for admins */}
-                            <Route path="*" element={<Navigate to="/adminMessage" />} />
-                        </>
-                    )}
-
-                    {/* Non-admin routes */}
-                    {!isAdmin && (
-                        <>
-                            <Route path="/" element={isLoggedIn ? <Home /> : <Login />} />
-                            <Route path="/login" element={<Login />} />
-                            <Route path="/register" element={<Registration />} />
-                            <Route
-                                path="/home"
-                                element={
-                                    <ProtectedRoute>
-                                        <Home />
-                                    </ProtectedRoute>
-                                }
-                            />
-                            <Route
-                                path="/matchList"
-                                element={
-                                    <ProtectedRoute>
-                                        <MatchList />
-                                    </ProtectedRoute>
-                                }
-                            />
-                            <Route
-                                path="/profile/:id"
-                                element={
-                                    <ProtectedRoute>
-                                        <Profile />
-                                    </ProtectedRoute>
-                                }
-                            />
-                            {/* Redirect admin-only routes to Home for non-admins */}
-                            <Route path="*" element={<Navigate to="/home" />} />
-                        </>
-                    )}
-
-                    <Route
-                        path="/leaderboard"
-                        element={
-                            <Leaderboard />
-                        }
-                    />
-
-                </Routes>
-            </Router>
-        </div>
-    );
+          <Route path="/leaderboard" element={<Leaderboard />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
--- src/pages/MatchList/MatchList.jsx
+++ src/pages/MatchList/MatchList.jsx
@@ -1,76 +1,81 @@
-import { Container, Typography, Box, Table,TableHead,TableBody,TableCell,TableRow ,TableContainer } from "@mui/material";
-import { useEffect } from "react";
+import { Container, Typography, Box, Table, TableHead, TableBody, TableCell, TableRow, TableContainer } from "@mui/material";
+import { useEffect, useState } from "react";
 import { FaFutbol } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
+import { getUpcomingGamesWithOdds } from "../../api/api";
 import "./MatchList.scss";
 
 export default function MatchList() {
-   let matchData = [,{date:"2024-20-20",time:"9:30",team1:"Canada",team2:"america",homeWin:"2.0",tie:"1.5",awayWin:"3.5"},{date:"2024-20-20",time:"9:30",team1:"Canada",team2:"america",homeWin:"2.0",tie:"1.5",awayWin:"3.5"},{date:"2024-20-20",time:"9:30",team1:"Canada",team2:"america",homeWin:"2.0",tie:"1.5",awayWin:"3.5"},{date:"2024-20-20",time:"9:30",team1:"Canada",team2:"america",homeWin:"2.0",tie:"1.5",awayWin:"3.5"},{date:"2024-20-20",time:"9:30",team1:"Canada",team2:"america",homeWin:"2.0",tie:"1.5",awayWin:"3.5"},{date:"2024-20-20",time:"9:30",team1:"Canada",team2:"america",homeWin:"2.0",tie:"1.5",awayWin:"3.5"},{date:"2024-20-20",time:"9:30",team1:"Canada",team2:"america",homeWin:"2.0",tie:"1.5",awayWin:"3.5"},{date:"2024-20-20",time:"9:30",team1:"Canada",team2:"america",homeWin:"2.0",tie:"1.5",awayWin:"3.5"}] //will get data from api funtion
-   const navigate = useNavigate();
-   useEffect(()=>{
-    matchData = formatMatchData(matchData);
+  const [matchData, setMatchData] = useState([]);
 
-   })
-   function formatMatchData(data){
-    return data
-   }
-   function navToDetails(matchId){
-    navigate("/MatchDetails",{matchID: matchId});
-   }
+  const navigate = useNavigate();
+  useEffect(() => {
+    let fetchData = async () => {
+      let data = await getUpcomingGamesWithOdds();
+      console.log(data);
+      setMatchData(data);
+    };
+
+    fetchData();
+  }, []);
+
+  function navToDetails(eventId) {
+    navigate(`/matchDetails/${eventId}`);
+  }
 
   return (
     <Container>
-      
       <Box textAlign="center" my={5}>
         <FaFutbol size={50} color="green" />
         <Typography variant="h4" component="h1" gutterBottom>
-         SportStake
+          SportStake
         </Typography>
-        <Typography  gutterBottom>
-          Live and upcoming Matches 
-        </Typography>
+        <Typography gutterBottom>Live and upcoming Matches</Typography>
       </Box>
       <Typography variant="h5" gutterBottom>
-          Upcoming Matches
-        </Typography>
+        Upcoming Matches
+      </Typography>
       <Box my={5} className="matchList">
-       
-        
-          <TableContainer style={{ maxHeight: 400 }}>
-        <Table className="table">
-            <TableHead >
-                <TableRow >
-                <TableCell  sx={{color:"white",fontSize:20 ,fontWeight:650}}>Date</TableCell>
-                
-                <TableCell sx={{color:"white",fontSize:20 ,fontWeight:650}} style ={{ paddingLeft:60}}>Match</TableCell>
-                <TableCell sx={{color:"white",fontSize:20 ,fontWeight:650}} >Home</TableCell>
-                <TableCell sx={{color:"white",fontSize:20 ,fontWeight:650}} >Tie</TableCell>
-                <TableCell sx={{color:"white",fontSize:20 ,fontWeight:650}} >Away</TableCell>
-                </TableRow>
+        <TableContainer style={{ maxHeight: 400 }}>
+          <Table className="table">
+            <TableHead>
+              <TableRow>
+                <TableCell sx={{ color: "white", fontSize: 20, fontWeight: 650 }}>Date</TableCell>
+
+                <TableCell sx={{ color: "white", fontSize: 20, fontWeight: 650 }} style={{ paddingLeft: 60 }}>
+                  Match
+                </TableCell>
+                <TableCell sx={{ color: "white", fontSize: 20, fontWeight: 650 }}>Home</TableCell>
+                <TableCell sx={{ color: "white", fontSize: 20, fontWeight: 650 }}>Tie</TableCell>
+                <TableCell sx={{ color: "white", fontSize: 20, fontWeight: 650 }}>Away</TableCell>
+              </TableRow>
             </TableHead>
             <TableBody>
-                {matchData.map((data,index)=>(
-                    <TableRow 
-                    key ={index}
-                    onClick={()=>navToDetails(data.id)}
-                    >
-                        <TableCell sx={{color:"white",fontSize:14 ,fontWeight:400}} ><div>{data.date}</div> {data.time} </TableCell>
-                        <TableCell className="matchCell" sx={{color:"white",fontSize:18 ,fontWeight:400, paddingRight:13}}  >{data.team1} VS {data.team2} </TableCell>
-                       
-                        <TableCell sx={{color:"orange",fontSize:18 ,fontWeight:400, paddingLeft:4}}> {data.homeWin}</TableCell>
-                        <TableCell sx={{color:"orange",fontSize:18 ,fontWeight:400}}>{data.tie}</TableCell>
-                        <TableCell sx={{color:"orange",fontSize:18 ,fontWeight:400}}>{data.awayWin}</TableCell>
+              {matchData.map((data) => (
+                <TableRow key={data.eventId} onClick={() => navToDetails(data.eventId)}>
+                  <TableCell sx={{ color: "white", fontSize: 14, fontWeight: 400 }}>
+                    <div>{data.commenceTime}</div>
+                  </TableCell>
+                  <TableCell className="matchCell" sx={{ color: "white", fontSize: 18, fontWeight: 400, paddingRight: 13 }}>
+                    {data.homeTeam} VS {data.awayTeam}{" "}
+                  </TableCell>
 
-                    </TableRow>
-                
-                ))}
-
+                  <TableCell sx={{ color: "orange", fontSize: 18, fontWeight: 400, paddingLeft: 4 }}>
+                    {" "}
+                    {data.h2hOdds.find((odd) => odd.name === data.homeTeam).price}
+                  </TableCell>
+                  <TableCell sx={{ color: "orange", fontSize: 18, fontWeight: 400 }}>
+                    {data.h2hOdds.find((odd) => odd.name === "Draw").price}
+                  </TableCell>
+                  <TableCell sx={{ color: "orange", fontSize: 18, fontWeight: 400 }}>
+                    {data.h2hOdds.find((odd) => odd.name === data.awayTeam).price}
+                  </TableCell>
+                </TableRow>
+              ))}
             </TableBody>
-        </Table>
+          </Table>
         </TableContainer>
-        </Box>
-     
-      </Container>
-   
+      </Box>
+    </Container>
   );
 }
